d3babf4f4d250a1f470848c605832a78052e107d
Kate-13 <kate_boyko@mail.ru>
2020-04-11 21:34:31 +0300

    feat: add function to implement Abbreviation

--- src/main/kotlin/subtask3/Abbreviation.kt
+++ src/main/kotlin/subtask3/Abbreviation.kt
@@ -4,6 +4,19 @@ class Abbreviation {
 
     // TODO: Complete the following function
     fun abbreviationFromA(a: String, b: String): String {
-        throw NotImplementedError("Not implemented")
+      var result = ""
+        var reg = ""
+        a.forEach { i ->
+            if (i.isUpperCase()){
+                reg += i
+            } else {
+                reg += i.toUpperCase() +"?"
+            }
+        }
+        val regex = reg.toRegex()
+        if ( regex.matchEntire(b) != null) result = "YES"
+        else result = "NO"
+
+return result
     }
 }
