4f6a9426f2e3b83b1c465621043c3b9dff522ba9
Kate-13 <kate_boyko@mail.ru>
2020-04-12 21:39:53 +0300

    feat: add function to implement Pangram

--- src/main/kotlin/subtask4/Pangram.kt
+++ src/main/kotlin/subtask4/Pangram.kt
@@ -4,6 +4,39 @@ class Pangram {
 
     // TODO: Complete the following function
     fun getResult(inputString: String): String {
-        throw NotImplementedError("Not implemented")
+        val vowels = listOf('a','e','i','o','u','y')
+        val cons = listOf('q', 'w', 'r', 't', 'p', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm')
+        val res = mutableListOf<String>()
+        var rs = ""
+        val pangram = ('a'..'z').all {inputString.contains(it, ignoreCase = true)}
+        val word = inputString.split(" ").map { it.trim() }
+        var arr = vowels
+        if (!pangram) {
+            arr = cons
+        }
+        word.forEach { w ->
+            var c = 0
+            var nw = ""
+
+            w.forEach { i ->
+            val isletter = arr.contains(i.toLowerCase())
+
+            if (isletter)  {
+                c = c+1
+                nw += i.toUpperCase()
+            } else {
+                nw += i
+            }
+        }
+        if (nw != "")
+            res.add(c.toString()+nw)
+        }
+        res.sortBy { it[0] }
+        rs = res.joinToString(separator = " ")
+
+        return rs
     }
+
 }
+
+
