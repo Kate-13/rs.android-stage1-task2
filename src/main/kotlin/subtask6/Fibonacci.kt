ec80da336e5206cf9c85e562935db90f5004f172
Kate-13 <kate_boyko@mail.ru>
2020-04-13 15:59:37 +0300

    feat: add function to implement Fibonacci

--- src/main/kotlin/subtask6/Fibonacci.kt
+++ src/main/kotlin/subtask6/Fibonacci.kt
@@ -4,6 +4,29 @@ class Fibonacci {
 
     // TODO: Complete the following function
     fun productFibonacciSequenceFor(n: Int): IntArray {
-        throw NotImplementedError("Not implemented")
+        val result = ArrayList<Int>()
+        val fibarr = mutableListOf(0,1)
+        var prod = 0
+        while (fibarr[fibarr.size-1] < n) {
+            fibarr.add(fibarr[fibarr.size-1] + fibarr[fibarr.size-2])
+        }
+
+        fibarr.forEachIndexed { i, el ->
+            if (i<(fibarr.size-1)) {
+                prod = fibarr[i] * fibarr[i+1]
+            }
+            if (prod == n) {
+                result.add(fibarr[i])
+                result.add(fibarr[i+1])
+                result.add(1)
+            } else if (prod > n && result.isEmpty() ) {
+                result.add(fibarr[i])
+                result.add(fibarr[i+1])
+                result.add(0)
+              }
+
+            }
+        return result.toIntArray()
+        }
     }
-}
+
