460c4f9d028107b5b4d3bd5c3c5c4486b19bc953
Kate-13 <kate_boyko@mail.ru>
2020-04-13 16:42:23 +0300

    feat: add function to implement Blocks

--- src/main/kotlin/subtask5/Blocks.kt
+++ src/main/kotlin/subtask5/Blocks.kt
@@ -1,11 +1,30 @@
 package subtask5
 
+import java.time.DateTimeException
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 import kotlin.reflect.KClass
+
 
 class Blocks {
 
     // TODO: Complete the following function
     fun getData(blockA: Array<Any>, blockB: KClass<*>): Any {
-        throw NotImplementedError("Not implemented")
+        var res: Any = 0
+        if (blockB == String::class) {
+            val r = blockA.filterIsInstance<String>()
+            res = r.joinToString(separator = "")
+        }
+        if (blockB == Int::class) {
+            res = blockA.filterIsInstance<Int>().sum()
+        }
+        if (blockB == LocalDate::class) {
+            val date = blockA.filterIsInstance<LocalDate>().max() ?: throw DateTimeException("Wrong Date")
+            val form = DateTimeFormatter.ofPattern("dd.MM.yyyy")
+            res = date.format(form)
+        }
+       return res
     }
 }
+
+
